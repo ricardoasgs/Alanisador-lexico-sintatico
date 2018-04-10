e4192b947d1333e968e26a411ffa5f23c7f64ff8
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-04-10 09:26:33 -0300

    Initial commit

---
+++ src/main/java/com/analisador/arquivo/LeitorArquivo.java
@@ -0,0 +1,39 @@
+package com.analisador.arquivo;
+
+import java.io.*;
+
+public class LeitorArquivo {
+	
+	public String resultado;
+	
+	
+	public LeitorArquivo() {
+		this.resultado = "";
+	}
+
+
+	public String getResultado() {
+		return resultado;
+	}
+
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+
+
+	public String lerArquivo(String caminho) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(caminho));
+			while (br.ready()) {
+				this.resultado = this.resultado.concat(br.readLine());
+			}
+			br.close();
+		} catch (IOException ioe) {
+			System.out.println("Erro ao ler arquivo: "+ioe);
+			this.resultado = "Erro";
+		}
+		return this.resultado;
+	}
+
+}
---
+++ src/main/java/com/analisador/main/Main.java
@@ -0,0 +1,37 @@
+package com.analisador.main;
+
+import com.analisador.analisador.Analisador;
+import com.analisador.arquivo.LeitorArquivo;
+import com.analisador.auxiliar.Auxiliar;
+import com.analisador.config.ConfigPath;
+
+import com.analisador.lista.Lista;
+import com.analisador.planilha.Planilha;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Auxiliar aux = new Auxiliar();
+		
+		LeitorArquivo leitor = new LeitorArquivo();
+		
+		String string = leitor.lerArquivo(ConfigPath.INPUT_PATH_WINDOWS+ConfigPath.INPUT_FILENAME);
+
+		Lista lista = aux.splitTokens(string);
+		
+		lista.VericarLista();
+		
+		Analisador automato = new Analisador();
+		
+		automato.analisarLista(lista);
+		
+		Planilha plan = new Planilha();
+		plan.expExcel(ConfigPath.OUTPUT_PATH_WINDOWS+ConfigPath.OUTPUT_FILENAME, lista );
+		
+		//lista.Print();
+				
+
+	}
+
+}
